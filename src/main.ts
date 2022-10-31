ab9dc3303b26e58be8749ab85c9721897545b8c5
raiper34 <raipergm34@gmail.com>
2022-10-31 23:06:34 +0100

    feature(): add methods to find all parent and child nodes of given node

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,29 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+
+/**
+ * Class to transform and manipulate tree like structures
+ */
 export class TreeUtils {
 
+  /**
+   * Convert list to tree like structure
+   * @param list list of objects (object need to have id and parentId)
+   * @param parentId id of parent node
+   */
   list2Tree(list: any[], parentId: any = null): any[] {
     return TreeUtils.deepCopy(list)
-      .filter((item: any) => item.parentId === parentId)
-      .map((item: any) => ({
-        ...item,
-        children: this.list2Tree(list, item.id)
-      }));
+        .filter((item: any) => item.parentId === parentId)
+        .map((item: any) => ({
+          ...item,
+          children: this.list2Tree(list, item.id)
+        }));
   }
 
+  /**
+   * Convert tree like structure to list
+   * @param tree tree of objects (object need to have child property and parentId)
+   * @param parentId
+   */
   tree2List(tree: any[], parentId: any = null): any[] {
     return TreeUtils.deepCopy(tree).reduce((acc: any, curr: any) => {
       const {children, ...rest} = curr;
@@ -65,8 +79,44 @@ export class TreeUtils {
     tree.forEach(item => this.editNode(item.children, id, data));
   }
 
+  findAllChildrenNodes(tree: any[], id: any): any[] {
+    const node = this.findTreeNodeById(tree, id);
+    return node ? [
+      ...node.children,
+      ...node.children.reduce((acc: any, curr: any) => ([...acc, ...this.getChildrenNodes(curr)]), [])
+    ] : [];
+  }
+
+  private getChildrenNodes(node: any): any[] {
+    return [
+      ...node.children,
+      ...node.children.reduce((acc: any, curr: any) => ([...acc, ...this.getChildrenNodes(curr)]), [])
+    ];
+  }
+
+  findAllParentNodes(tree: any[], id: any): any[] {
+    const parents = [];
+    let parent = this.findNodeParent(tree, id);
+    while (parent) {
+      parents.push(parent);
+      parent = this.findNodeParent(tree, parent.id);
+    }
+    return parents.reverse();
+  }
+
+  findNodeParent(tree: any[], id: any, parent: any = null): any {
+    const node = tree.find(item => item.id === id);
+    if (node) {
+      return parent;
+    }
+    return tree.reduce((acc, curr) =>
+        acc || this.findNodeParent(curr.children || [], id, curr), null
+    );
+  }
+
   private static deepCopy(obj: any): any {
     return JSON.parse(JSON.stringify(obj));
   }
 }
+
 /* eslint-enable @typescript-eslint/no-explicit-any */
