1bb6d0f5e3f81b849e0c5f7f18a0566affd8cb29
raiper34 <raipergm34@gmail.com>
2025-01-17 21:42:26 +0100

    chore(test): replace jasmine and packages around with vitest

--- test/main.spec.ts
+++ test/main.spec.ts
@@ -1,3 +1,4 @@
+import {beforeEach, describe, expect, it} from 'vitest'
 import {TreeUtils} from "../src";
 
 function getMockedList(parentCustomId: any = null): any[] {
---
+++ vitest.config.ts
@@ -0,0 +1,12 @@
+import {defineConfig} from "vitest/config";
+
+export default defineConfig({
+    plugins: [],
+    test: {
+        include: ["test/**/*.spec.ts"],
+        coverage: {
+            include: ["src/**"],
+            reporter: ['html', 'lcov']
+        },
+    },
+});
